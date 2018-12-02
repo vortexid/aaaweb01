373cbdc3ab182533d24acc1844ddfbbfce80a1e2
aaa101dv <aaa101vvg@gmail.com>
2018-12-02 14:10:46 +0000

    Price handling

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 import psycopg2
 from  flask import Flask
 app = Flask(__name__)
@@ -5,17 +6,24 @@ app = Flask(__name__)
 conn = psycopg2.connect(host="localhost",database="student1",user="student1",password="student1")
 sql_prihod = """INSERT INTO prihodi (konto, komitent, naziv, iznos) VALUES (%s, %s, %s, %s);"""
 title="Student2";
+price=5.0
 
 @app.route('/<name>')
 def pay(name):
     cur = conn.cursor()
-    cur.execute(sql_prihod, ("7020",name,"Web prihodi", 10.0,));
+    cur.execute(sql_prihod, ("7020",name,"Prihodi od web stranice", price,));
     conn.commit()
-    return "<h1>"+title+"</h1></br>Hvala "+name+" !"
+    return "<h1>"+title+"</h1></br>Hvala "+name+u" - potrošili ste "+str(price)+" Kn !"
 
 @app.route("/")
 def home():
     return "<h1>"+title+"</h1>"
+
+@app.route("/cijena/<cijena>")
+def set_price(cijena):
+    global price
+    price=cijena
+    return "<p>Promijenili ste cijenu u "+cijena+" Kn.</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5001')
